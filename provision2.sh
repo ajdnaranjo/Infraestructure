8aae0b07aa2e908d673c9954ff95aabd8c82ed99
Juan Naranjo <ajdnaranjo@gmail.com>
2022-01-28 15:06:30 -0500

    Add files via upload
    ramp up

---
+++ provision2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+(sudo apt-get update;
+sudo apt-get install -y apache2;
+sudo service apache2 start;
+sudo apt-get install -y mariadb-client
+sudo apt-get install -y nodejs;
+sudo apt-get install -y nodejs-legacy;
+sudo npm install --no-optional -y npm@latest -g;
+sudo apt-get install -y npm;
+cd /var/www/html/movie-analyst-api;
+sudo node server.js &)| tee ./var/www/html/my.log
+
+
